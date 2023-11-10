e89fece3479afeaf959c941f8aed960d69cc4b17
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-11-10 11:07:42 -0500

    fix: Content-Type Header in HTML Response
    The content-type header was returning an empty string.

--- app/pkg/web/page_test.go
+++ app/pkg/web/page_test.go
@@ -23,3 +23,40 @@ func TestGetHeader(t *testing.T) {
 		})
 	}
 }
+
+func TestGetPageType(t *testing.T) {
+	tests := []struct {
+		name    string
+		headers map[string]string
+		want    string
+	}{
+		{"html-type", map[string]string{"content-type": "text/html"}, "text/html"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetPageType(tt.headers); got != tt.want {
+				t.Errorf("GetPageType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetPageTypeByExt(t *testing.T) {
+	tests := []struct {
+		name     string
+		pagePath string
+		want     string
+	}{
+		{"html", "page.html", "text/html; charset=utf-8"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GetPageTypeByExt(tt.pagePath)
+
+			if got != tt.want {
+				t.Errorf("GetPageTypeByExt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
