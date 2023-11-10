2d6dfdabab62b58f8a8a11637d43ecdbe510d4cb
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-11-10 11:33:39 -0500

    fix: 404 Response
    Set title and header to use code 404.

--- app/pkg/web/content.go
+++ app/pkg/web/content.go
@@ -4,7 +4,7 @@ const (
 	http301RedirectContent      = `<!DOCTYPE html><html><head><title>301 Moved Permanently</title></head><body><center><h1>301 Moved Permanently</h1></center><hr><center>CloudFront</center></body></html>`
 	http308RedirectContent      = `<!DOCTYPE html><html><head><title>308 Permanent Redirect</title></head><body><center><h1>308 Permanent Redirect</h1></center><hr><center>CloudFront</center></body></html>`
 	http401NotFoundContent      = `<!DOCTYPE html><html><head><title>401 Unauthorized</title></head><body><center><h1>401 Unauthorized</h1></center><hr><center>CloudFront</center></body></html>`
-	http404NotFoundContent      = `<!DOCTYPE html><html><head><title>301 Moved Permanently</title></head><body><center><h1>301 Moved Permanently</h1></center><hr><center>CloudFront</center></body></html>`
+	http404NotFoundContent      = `<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><center><h1>404 Not Found</h1></center><hr><center>CloudFront</center></body></html>`
 	http500InternalErrorContent = `<!DOCTYPE html><html><head><title>500 Internal Server Error</title></head><body><center><h1>500 Internal Server Error</h1></center><hr><center>CloudFront</center></body></html>`
 
 	// [Media Types](https://www.iana.org/assignments/media-types/media-types.xhtml)
