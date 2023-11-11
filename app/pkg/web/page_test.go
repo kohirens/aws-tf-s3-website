607ade99e0083c7a2c1ab368127011ac42a6e3d7
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-11-11 13:07:11 -0500

    mnt: Add Test For HTTP Responses
    Added unit test for the current set of HTTP responses.

--- app/pkg/web/page_test.go
+++ app/pkg/web/page_test.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -57,6 +58,76 @@ func TestGetPageTypeByExt(t *testing.T) {
 
 			if got != tt.want {
 				t.Errorf("GetPageTypeByExt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRespond301Or308(t *testing.T) {
+	fixedResponse := &Response{
+		StatusCode: 301,
+		Headers: map[string]string{
+			"Content-Type": "text/html; charset=utf-8",
+			"Location":     "https://www.example.com",
+		},
+	}
+	fixed308Response := &Response{
+		StatusCode: 308,
+		Headers: map[string]string{
+			"Content-Type": "text/html; charset=utf-8",
+			"Location":     "https://www.example.com",
+		},
+	}
+	tests := []struct {
+		name     string
+		method   string
+		location string
+		want     *Response
+	}{
+		{"301", "GET", "www.example.com", fixedResponse},
+		{"308", "POST", "www.example.com", fixed308Response},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Respond301Or308(tt.method, tt.location)
+
+			if got.StatusCode != tt.want.StatusCode {
+				t.Errorf("Respond301Or308() = %v, want %v", got.StatusCode, tt.want.StatusCode)
+			}
+
+			if !reflect.DeepEqual(got.Headers, tt.want.Headers) {
+				t.Errorf("Respond301Or308() = %v, want %v", got.Headers, tt.want.Headers)
+			}
+		})
+	}
+}
+
+// This suite of test ensure any refactoring of these methods leave the
+// required HTTP status code and recommended status message are left intact.
+// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404
+// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500
+func TestRespond401(t *testing.T) {
+	tests := []struct {
+		name       string
+		call       func() *Response
+		wantCode   int
+		wantStatus string
+	}{
+		{"401", Respond401, 401, "Unauthorized"},
+		{"404", Respond404, 404, "Not found"},
+		{"500", Respond500, 500, "Internal Server Error"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.call()
+
+			if got.StatusCode != tt.wantCode {
+				t.Errorf("Respond%v() = %v, want %v", tt.name, got.StatusCode, tt.wantCode)
+			}
+
+			if got.StatusCode != tt.wantCode {
+				t.Errorf("Respond%v() = %v, want %v", tt.name, got.Status, tt.wantStatus)
 			}
 		})
 	}
