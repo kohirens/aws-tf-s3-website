08c31bdf48dc9c126b8b988f2b92e436bc23592f
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-11-11 12:59:06 -0500

    chg: HTTP Response Media Types
    Updated the media types to use the recommended format suggested on the
    IETF Documents page for media types.

--- app/pkg/web/content.go
+++ app/pkg/web/content.go
@@ -7,12 +7,12 @@ const (
 	http404NotFoundContent      = `<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><center><h1>404 Not Found</h1></center><hr><center>CloudFront</center></body></html>`
 	http500InternalErrorContent = `<!DOCTYPE html><html><head><title>500 Internal Server Error</title></head><body><center><h1>500 Internal Server Error</h1></center><hr><center>CloudFront</center></body></html>`
 
-	// [Media Types](https://www.iana.org/assignments/media-types/media-types.xhtml)
-
-	contentTypeCSS  = "text/css; charset=utf-8"
-	contentTypeHtml = "text/html; charset=utf-8"
-	contentTypeJson = "application/json; charset=utf-8"
-	contentTypeJS   = "text/javascript; charset=utf-8"
-	contentTypePng  = "application/json; charset=utf-8"
+	// See [Media Types](https://www.iana.org/assignments/media-types/media-types.xhtml)
+	// Also see [IETF Media Types](https://www.rfc-editor.org/rfc/rfc9110.html#media.type)
+	contentTypeCSS  = "text/css;charset=utf-8"
+	contentTypeHtml = "text/html;charset=utf-8"
+	contentTypeJson = "application/json;charset=utf-8"
+	contentTypeJS   = "text/javascript;charset=utf-8"
+	contentTypePng  = "application/json;charset=utf-8"
 	contentTypeSvg  = "image/svg+xml"
 )
