3fc14408dadf942a08fabd670677f8faa5c692d6
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-11-22 17:39:34 -0500

    fix: Append Lambda Environment Variables
    Add the region to the aws command to update the function environment
    variables.

--- files/lambda-add-env-var.sh
+++ files/lambda-add-env-var.sh
@@ -4,9 +4,14 @@ set -e
 
 LAMBDA="${1}"
 append_vars="${2}"
+region="${3}"
 
+aws lambda get-function-configuration --region "${region}" --function-name "test-kohirens-com"
 CURRENT_VARIABLES=$(aws lambda get-function-configuration --function-name "${LAMBDA}" | jq '.Environment.Variables')
 #NEW_VARIABLES=$(echo ${CURRENT_VARIABLES} | jq '. += {"kick":"'$(getrandom 8)'"}')
-NEW_VARIABLES=$(echo ${CURRENT_VARIABLES} | jq ". += ${append_vars}")
+NEW_VARIABLES=$(echo "${CURRENT_VARIABLES}" | jq ". += ${append_vars}")
 
-aws lambda update-function-configuration --function-name "${LAMBDA}" --environment "{\"Variables\":${NEW_VARIABLES}}"
+aws lambda update-function-configuration \
+     --region "${region}" \
+     --function-name "${LAMBDA}" \
+     --environment "{\"Variables\":${NEW_VARIABLES}}"
