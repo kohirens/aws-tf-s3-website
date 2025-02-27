7f162f7b4fd38ae95f9b19ab9452f7573332e308
Khalifah Shabazz <shabazzk@gmail.com>
2025-02-26 23:15:29 -0500

    chg: Handle POST & PUT To Lambda Backend
    With changing over the OAC for Lambda we need to handle POST and PUT
    requests to the Lambda backend. This is because the OAC will not
    forward these requests to the Lambda backend without the SHA256
    of the body computed and added to the requests header
    "x-amz-content-sha256."

--- files/index.js
+++ files/index.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 /**
  * Preserver the server Host header as it is passed along to an origin.
  *
@@ -7,13 +9,45 @@
  * @param event
  * @returns {Promise<Request|IDBRequest<any>|((name: string, callback: LockGrantedCallback) => Promise<any>)|((type?: WakeLockType) => Promise<WakeLockSentinel>)|((name: string, options: LockOptions, callback: LockGrantedCallback) => Promise<any>)>}
  */
-function handler(event) {
-    var request = event.request;
+async function handler(event) {
+    let request = event.request;
 
     request.headers["viewer-host"] = request.headers.host;
     request.headers["distribution-domain"] = {
         value: event.context.distributionDomainName,
     };
 
+    // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-lambda.html#create-oac-overview-lambda
+    let method = request.method.toLowerCase();
+    if (method === "post" || method === "put") {
+        hash = await sha256FromBody(request);
+        request.headers["x-amz-content-sha256"] = [{            value: hash }];
+    }
     return request;
+}
+
+/**
+ * Compute the SHA256 of the POST|PUT body.
+ *
+ * If you use PUT or POST methods with your Lambda function URL, your users must
+ * compute the SHA256 of the body and include the payload hash value of the
+ * request body in the x-amz-content-sha256 header when sending the request to
+ * CloudFront. Lambda doesn't support unsigned payloads.
+ * @param req
+ * @returns {Promise<unknown>}
+ */
+async function sha256FromBody(req) {
+    return new Promise((resolve, reject) => {
+        let body = "";
+        req.on("data", chunk => {
+            body += chunk;
+        });
+        req.on("end", () => {
+            const hash = crypto.createHash("sha256").update(body).digest("hex");
+            resolve(hash);
+        });
+        req.on("error", error => {
+            reject(error);
+        });
+    });
 }
