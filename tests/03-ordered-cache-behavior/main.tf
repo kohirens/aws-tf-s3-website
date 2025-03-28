3b4207f4a23653414497253041ed50416ad3d2cf
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2025-03-28 15:12:33 -0400

    chore: Clean Up Broken Unit Test
    Removed call to resouces replace by another.

--- tests/03-ordered-cache-behavior/main.tf
+++ tests/03-ordered-cache-behavior/main.tf
@@ -5,7 +5,6 @@ variable "domain_name" {
 locals {
   test_page    = "test-03.html"
   html_fixture = "tests/testdata/${local.test_page}"
-  filename1    = "${path.module}/${replace(var.domain_name, ".", "-")}.json"
 }
 
 resource "aws_s3_object" "upload_fixture_webpage" {
@@ -14,24 +13,6 @@ resource "aws_s3_object" "upload_fixture_webpage" {
   key          = local.test_page
   source       = local.html_fixture
   etag         = filemd5(local.html_fixture)
-}
-
-# check the url
-resource "null_resource" "response_1" {
-  triggers = {
-    domain_name = var.domain_name
-  }
-
-  provisioner "local-exec" {
-    command = "${path.module}/../testdata/test-endpoint.sh 'https://${var.domain_name}/${local.test_page}' > ${local.filename1}"
-  }
-}
-
-# get the response
-data "local_file" "response_1" {
-  depends_on = [null_resource.response_1]
-
-  filename = local.filename1
 }
 
 data "http" "domain_name" {
